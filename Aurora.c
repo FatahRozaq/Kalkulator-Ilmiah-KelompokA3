d159f6298bc666a86c397760383f0b6132569599
Mutia Hardita <119656945+flauuralix@users.noreply.github.com>
2023-02-06 01:15:39 +0700

    Menambahkan file Aurora.c dan Aurora.h

---
+++ Aurora.c
@@ -0,0 +1,20 @@
+/*         
+File Name		: Aurora.c
+Description		: Tugas Proyek 2 
+Author			: Athalie Aurora Puspanegara
+*/
+
+/* ========== Header File ========== */
+
+#include "Aurora.h" 
+#include <stdio.h>
+
+/* ======= End of Header File ====== */
+
+
+float Bagi(float bil1, float bil2){
+	float hasil;
+	
+	hasil = (bil1/bil2);
+	return hasil;
+}
---
+++ Aurora.h
@@ -0,0 +1,8 @@
+// Header Aurora
+
+#ifndef Aurora_H
+#define Aurora_H
+
+float Bagi(float bil1, float bil2);
+
+#endif
