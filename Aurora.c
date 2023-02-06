36594d4b541ca42944047a07ce858617b7732f60
Athalie Aurora <119656945+flauuralix@users.noreply.github.com>
2023-02-06 11:56:16 +0700

    Update Aurora.c
    Update Aurora.c

--- Aurora.c
+++ Aurora.c
@@ -7,15 +7,20 @@ Author			: Athalie Aurora Puspanegara
 /* ========== Header File ========== */
 
 #include "Aurora.h" 
+#include "Nisrina.h"
 #include <stdio.h>
 
 /* ======= End of Header File ====== */
 
 
-float Bagi(float bil1, float bil2){
+float Bagi(){
 	float hasil;
+	float bil1, bil2;
+	
+	InputBilFloat(&bil1,&bil2);	
 	
 	hasil = (bil1/bil2);
 	return hasil;
 }
 
+	
