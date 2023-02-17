92da4b2a0e799bc9c3557c1bfe498dddae3123d0
Jonanda27 <Jonandapantas@gmail.com>
2023-02-17 17:21:33 +0700

    Update Modulus,  Akar, KonvertLuas, KonvertSuhu, Logaritma dan jojo ganteng

--- jojo.h
+++ jojo.h
@@ -1,6 +1,25 @@
 #ifndef jojo_h
 #define jojo_h
 
-int modulus();
+// Operasi Modulus
+int Modulus(int bil1, int bil2);
+void panggilModulus();
 
+// Operasi Akar
+float Akar(float bil1);
+void panggilAkar();
+
+// Operasi Logaritma
+float Logaritma(float bil1);
+void panggilLogaritma();
+
+// Konvert Suhu
+float Suhu(float bil2);
+void konvertSuhu();
+
+// Konvert Luas
+float Luas(float bil2);
+void konvertLuas();
+
+void InputDataModulus(int *bilMod1,int *bilMod2, char kalimat[]);
 #endif
