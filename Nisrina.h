4e092960a61386aec46204ace2ecdbcd54dd1507
nisrinawafaz <nisrinawafaz@gmail.com>
2023-02-17 23:03:08 +0700

    update header nisrina

--- Nisrina.h
+++ Nisrina.h
@@ -3,9 +3,24 @@
 
 float Penjumlahan ();
 
+float Pangkat();
+
 void InputBil(float *bil1, float *bil2);
 
 void TampilHasilFloat(float hasil, char nama[]);
 
 void TampilHasilInt(int hasil, char nama[]);
+
+void CalStfc();
+
+void MenuCalStfc();
+
+void CalStd();
+
+void TataCara();
+
+void CaraCalStd();
+
+void Credit();
+
 #endif
