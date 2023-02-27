f6f39f9ce0d92ab9e66b7cd34f06e0a0131e63cb
FatahRozaq <fatah.rozaq20@gmail.com>
2023-02-27 15:05:51 +0700

    Update referensi modul

--- fatah.h
+++ fatah.h
@@ -8,16 +8,19 @@
 double Perkalian (double bil1, double bil2);
 
 // Modul Konvert Panjang
+// Referensi : https://github.com/cengizhankose/unit_converter/blob/master/main.c
 void pilihanSatuan(); // panggil modul ini ke main
 float nilaiSatuan(int opsi, float nilai);
 float convertPanjang(int opsi, float nilaiTetap);
 void tampilHasilConvert(int opsi, float *hasil);
 
 // Modul Deret Geometri
+// Referensi : https://github.com/palsiddharth091/APGPCALC/blob/master/APGP1.cpp
 void deretGeometri(); // panggil modul ini ke main
 float hasilGeometri(float sukuPertama2, float rasio2, int jumlahElemen2, int i);
 
 // Modul Statistika
+// Referensi : https://github.com/apieceofCAKE/array_basic_functions/blob/master/main.c
 void statistika(); // panggil modul ini ke main
 int array_modus(int *array, int ukuran);
 void array_sort(int *array, int ukuran);
