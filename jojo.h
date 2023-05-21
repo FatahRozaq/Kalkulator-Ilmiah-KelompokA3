ea86e5d2abf1045af4160c6e0fd3c9c0f85f5c13
Jonanda27 <Jonandapantas@gmail.com>
2023-05-22 04:09:22 +0700

    update

--- jojo.h
+++ jojo.h
@@ -3,37 +3,42 @@
 
 // Operasi Modulus
 double Modulus(double bil1, double bil2);
-
 void panggilModulus();
+void InputDataModulus(int *bilMod1,int *bilMod2, char kalimat[]);
+// Untuk melakukan perhitungan modulus
+// referensi : -
 
 // Operasi Akar
-
 void panggilAkar();
-
 float Akar2(float bil1);
+// untuk melakukan perhitungan akar
+// referensi : -
 
 // Operasi Logaritma
 float Logaritma(float bil1);
-
 void panggilLogaritma();
+// Untuk melakukan perhitungan logaritma berbasis 10
+// referensi : -
 
 // Konvert Suhu
 float Suhu(float bil2);
-
 void konvertSuhu();
+// fitur tambahan untuk melakukan konvert suhu
+// referensi : -
 
 // Konvert Luas
 float Luas(float bil2);
-
 void konvertLuas();
+// fitur tambahan untuk melakukan kinvert luas
+// referensi : -
 
-void InputDataModulus(int *bilMod1,int *bilMod2, char kalimat[]);
 
 // Logaritma Bebas
 void LogaritmaBebas();
-
 double HitungLogBebas(double basis2, double angka2);
 double hiLog(double x);
-
 double HiLog(double x);
+// untuk melakukan perhitungan logaritma dinamis
+
+
 #endif
